f6a2ff7f34fb00123ae9c824c58924048fd121c3
Leandro (Cerberus1746) Benedet Garcia <cerberus1746@gmail.com>
2024-08-24 12:56:28 -0300

    Started developing demos

---
+++ sample_projects/webgl_triangle/src/main.js
@@ -0,0 +1,23 @@
+import { asyncRun } from "./py-worker.js";
+
+const script = `
+from FunEnginePy.math import Vector3Int
+Vector3Int(55, 12, 32).magnitude()
+`;
+
+async function main() {
+  try {
+    const { results, error } = await asyncRun(script);
+    if (results) {
+      console.log("pyodideWorker return results: ", results);
+    } else if (error) {
+      console.log("pyodideWorker error: ", error);
+    }
+  } catch (e) {
+    console.log(
+      `Error in pyodideWorker at ${e.filename}, Line: ${e.lineno}, ${e.message}`,
+    );
+  }
+}
+
+main();
---
+++ sample_projects/webgl_triangle/src/webgl_triangle/__main__.py
@@ -0,0 +1,3 @@
+# This file is what will be the main source for our project, what actually
+# execute stuff
+# This should work regardless of the execution method.
