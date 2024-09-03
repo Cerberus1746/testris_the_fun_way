31cfed693e99eacc1a93c20dc8a6afd9475d41bf
Leandro (Cerberus1746) Benedet Garcia <cerberus1746@gmail.com>
2024-09-03 00:28:53 -0300

    fixed cmake

---
+++ editor/src/fun-engine-editor/__main__.py
@@ -0,0 +1,10 @@
+from FunEnginePy import Main
+
+from .__version__ import __version__
+
+software: dict[str, str | int] = {
+    "name": "FunEngine Editor",
+    "version": __version__,
+}
+
+main_instance: Main = Main(**software)
